0031c8df7ef9c5063636e3d584efb92645e63ffc
17seannnn <17seannnn@gmail.com>
2022-06-28 17:50:26 +0300

    refactor: resize borders

--- src/curses.h
+++ src/curses.h
@@ -5,10 +5,10 @@
 
 struct Curses {
     enum win_size {
-        border_win_width  = 16,
+        border_win_width  = 12,
         border_win_height = 17,
 
-        game_win_width  = 14,
+        game_win_width  = 10,
         game_win_height = 16,
 
         next_win_width  = 5,
